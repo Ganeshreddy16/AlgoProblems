4b857ff9f0aed30bf1a0350e58bbae1fc875dbf4
Ganesh Reddy <reddyganesh125@gmail.com>
2021-09-13 03:30:40 +0530

    Added:WelcomingMessage

--- src/main/java/org/blz/anagram/Anagram.java
+++ src/main/java/org/blz/anagram/Anagram.java
@@ -4,6 +4,6 @@ public class Anagram
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Anagram Game" );
+        System.out.println( "Welcome to Algorithm Problems" );
     }
 }
